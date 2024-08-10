d12df858a3154af1bc08ecb0402886099d323fa1
Jesús Armenta-Segura <138358307+JesusASmx@users.noreply.github.com>
2024-08-09 18:46:30 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'SimpleNERtagger_ES',         # How you named your package folder (MyLib)
   packages = ['SimpleNERtagger_ES'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
-  license='', #'MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This is an on-development NER-tagger in spanish. Regardless other NER-taggers in the market/wilds/Huggingface, this one will include non-common tags. Also, it will work with three kind of methods at the same time: deterministic and based on regular expressions (v0.1), transformer-based fine-tunned over preexisting data (v0.2) and LLM-prompt-based leveraging few-shot learning techniques (v0.3). Este es un NER-tagger en español en desarrollo. A diferencia de otros NER-taggers existentes en el mercado/Huggingface/por-ahí-merito, este incluye tags poco comunes. Además, su versión final trabajará con tres tipos de métodos al mismo tiempo: determinísticos basados en expresiones regulares (v0.1), basados en transformers con ajustamiento fino sobre datos preexistentes (v0.2) y basados en aprendizaje few-shot basados en prompts sobre LLMs (v0.3).',   # Give a short description about your library
   author = 'Jesús Armenta-Segura',                   # Type in your name
   author_email = 'jesus.jorge.armenta@gmail.com',      # Type in your E-Mail
@@ -19,7 +19,7 @@ setup(
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
 
-    'License :: OSI Approved :: ',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
 
     'Programming Language :: Python :: 3.10',      #Specify which pyhton versions that you want to support
   ],
