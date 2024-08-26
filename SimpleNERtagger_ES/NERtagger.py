7137b2ef25f021a4e135d096573fde21d934714f
Jesús Armenta-Segura <138358307+JesusASmx@users.noreply.github.com>
2024-08-25 19:44:29 -0600

    Update NERtagger.py

--- SimpleNERtagger_ES/NERtagger.py
+++ SimpleNERtagger_ES/NERtagger.py
@@ -27,15 +27,18 @@ class NER_tagger:
   #Corre el NER-tagger del transformer.
   #Reunifica las palabras partidas por el auto-tokenizador.
   #Finalmente regresa los tags en formato DataFrame con columnas tag, palabra, start y end. Si no hay palabras taggeadas, regresa el diccionario con listas vacías como valor.
-  def tag_transformer(self, text, tag, original_tags):
+  def tag_transformer(self, text, dict_tag):
     ## PASO 1: Los taggers usuales regresan tags en el formato BIO.
-    dict_tags = [{'tag':tag, 'palabra':x['word'], 'start':x['start'],'end':x['end'], 'index':x['index']} for x in self.tags if x['entity'] in original_tags] # self.tags se define en otras funciones, y consiste en el resultado del BERT-NERTagger ya ejecutado sobre el texto.
+    dict_tags = []
+    for targ in dict_tag:
+      dict_tags += [{'tag':targ, 'palabra':x['word'], 'start':x['start'],'end':x['end'], 'index':x['index']} for x in self.tags if x['entity'] in dict_tag[targ]] # self.tags se define en otras funciones, y consiste en el resultado del BERT-NERTagger ya ejecutado sobre el texto.
 
     dict_tags = sorted(dict_tags, key=lambda x: x["index"]) #Ordenados por orden de aparición.
 
     # Reunificamos palabras partidas por el tokenizador de BERT: "Pa, ##lab ##ra" ---> "Palabra"
-    z = 0 #Con esta variable, nos ahorramos un chorro de recursos para evitar iterar sobre todo dict_tags una-y-otra-vez.
+    z = 0 #Con esta variable, nos ahorramos un chorro de recursos para evitar itsintomas trigliceridos altoserar sobre todo dict_tags una-y-otra-vez.
     to_delete = []
+
     for y in dict_tags: # Iterando en orden de aparición
       if y['palabra'][:2] == "##": # Debido a la naturaleza del tokenizador de BERT, esto nunca se alcanzará en la primera iteración, a menos que un chistocito arruine el programa metiendo un texto que inicie con "##".
         try: # Sólo podría fallar en la primera iteración del for.
@@ -54,7 +57,7 @@ class NER_tagger:
     for x in to_delete:
       dict_tags.remove(x)
 
-    final_tags = {'tag':[tag for x in dict_tags], 'palabra':[x['palabra'] for x in dict_tags], 'start':[x['start'] for x in dict_tags],'end':[x['end'] for x in dict_tags]} # dict_tags pero sin el index de token y en formato de DataFrame.
+    final_tags = {'tag':[x['tag'] for x in dict_tags], 'palabra':[x['palabra'] for x in dict_tags], 'start':[x['start'] for x in dict_tags],'end':[x['end'] for x in dict_tags]} # dict_tags pero sin el index de token y en formato de DataFrame.
 
     return final_tags
 
@@ -118,14 +121,12 @@ class NER_tagger:
     return final_tags
 
 
-  def NERtagging(self, texto, enmascarar, mails_tag, tels_tag, nombres_tag, lugares_tag):
+  def NERtagging(self, texto, unir_tags_iguales, mails_tag, tels_tag, nombres_tag, lugares_tag):
     # Variables referenciadas dentro de todos los procesos de etiquetamiento:
-    self.enmascarar = enmascarar # Si no es false, regresa también texto enmascarado. Si sí lo es, sólo regresa la lista de tags y el texto sin alterar.
+    self.separate_tags = unir_tags_iguales # Esto fue un error de dislexia. En un universo paralelo yo nombré esta variable self.unir_tags_iguales.
 
     final_texto = texto
 
-    if self.transformer:
-      self.tags = self.nlp(texto)
 
     if mails_tag:
       mail_tags = self.tag_emails(texto, str(mails_tag))
@@ -137,29 +138,56 @@ class NER_tagger:
     else:
       tel_tags = {'tag':[], 'palabra':[], 'start':[],'end':[]}
 
-    if nombres_tag and self.transformer:
-      nom_tags = self.tag_transformer(texto, str(nombres_tag), ['B-ORG','B-PER','I-ORG','I-PER']) #Descartados por ahora: 'B-MISC','I-MISC',
+    if self.transformer:
+      self.tags = self.nlp(texto)
+      transf_tags = {}
+      if lugares_tag:
+        transf_tags[(str(lugares_tag))] = ['B-LOC','I-LOC']
+      if nombres_tag:
+        transf_tags[(str(nombres_tag))] = ['B-ORG','B-PER','I-ORG','I-PER']
+      if len(transf_tags) > 0:
+        trans_tags = self.tag_transformer(texto, transf_tags) #Descartados por ahora: 'B-MISC','I-MISC',
+      else:
+        trans_tags = {'tag':[], 'palabra':[], 'start':[],'end':[]}
     else:
-      nom_tags = {'tag':[], 'palabra':[], 'start':[],'end':[]}
+      trans_tags = {'tag':[], 'palabra':[], 'start':[],'end':[]}
 
-    if lugares_tag and self.transformer:
-      lug_tags = self.tag_transformer(texto, str(lugares_tag), ['B-LOC','I-LOC']) #Descartados por ahora: 'B-MISC','I-MISC',
-    else:
-      lug_tags = {'tag':[], 'palabra':[], 'start':[],'end':[]}
-
-
-    all_tags = {'tag':mail_tags['tag']+tel_tags['tag']+nom_tags['tag']+lug_tags['tag'],
-                'palabra':mail_tags['palabra']+tel_tags['palabra']+nom_tags['palabra']+lug_tags['palabra'],
-                'start':mail_tags['start']+tel_tags['start']+nom_tags['start']+lug_tags['start'],
-                'end':mail_tags['end']+tel_tags['end']+nom_tags['end']+lug_tags['end']}
+    all_tags = {'tag':mail_tags['tag']+tel_tags['tag']+trans_tags['tag'],                 #+nom_tags['tag']+lug_tags['tag'],
+                'palabra':mail_tags['palabra']+tel_tags['palabra']+trans_tags['palabra'], #+nom_tags['palabra']+lug_tags['palabra'],
+                'start':mail_tags['start']+tel_tags['start']+trans_tags['start'],         #+nom_tags['start']+lug_tags['start'],
+                'end':mail_tags['end']+tel_tags['end']+trans_tags['end']}                 #+nom_tags['end']+lug_tags['end']}
 
     all_tags = pd.DataFrame(all_tags).sort_values(by=['start']).reset_index().drop(["index"], axis=1)
+
+
+    if self.separate_tags:
+      to_drop = []
+      la_palabra = ''
+      inicier = 0
+      for x in range(len(all_tags)):
+        if all_tags['tag'][x] == all_tags['tag'][min(x+1,len(all_tags)-1)] and all_tags['end'][x] == all_tags['start'][min(x+1,len(all_tags)-1)]-1: # Tiene sentido iniciar el algoritmo de unificación.
+          la_palabra += ' '+all_tags['palabra'][x]
+          to_drop.append(min(x+1,len(all_tags)))
+          print(la_palabra, inicier)
+        elif len(la_palabra)>0:
+          all_tags.loc[inicier, 'palabra'] = la_palabra + ' '+all_tags['palabra'][x]
+          all_tags.loc[inicier, 'end'] = all_tags['end'][x]
+
+          la_palabra = ''
+          inicier = min(x+1,len(all_tags))
+        else:
+          inicier = min(x+1,len(all_tags))
+
+      display(all_tags)
+      all_tags = all_tags.drop(to_drop)
+
+
     all_tags = all_tags[all_tags["palabra"].str.len() > 1].reset_index().drop(["index"], axis=1) # Sólo conservar palabras con más de 1 caracter.
 
     # Tiempo de remover tags solapados. E.g. Juanito@Tokio.jp tiene los tags [NOMBRE]@[LUGAR].jp y [MAIL] al mismo tiempo
     jerarquia = {str(mails_tag):3, str(tels_tag):2, str(nombres_tag):1, str(lugares_tag):0} # Añadir a futuras versiones la posibilidad de manipular este orden.
     all_tags['jerarquía'] = all_tags['tag'].apply(lambda x:jerarquia[x]) # Genera columna auxiliar para eliminar empalmes en términos de las jerarquías de los tags.
-    
+
     to_drop = [] # empalmes a eliminar, guardados por su index.
     for x in range(len(all_tags)-1):
       if all_tags['start'][x+1] < all_tags['end'][x]: # Encontrando empalmes (pero no los de Santiago NL).
@@ -170,11 +198,13 @@ class NER_tagger:
 
     all_tags = all_tags.drop(to_drop).reset_index().drop(["index", "jerarquía"], axis=1) # También quitamos la columna de Jerarquía, que la usamos sólo de manera auxiliar.
 
+
     # Enmascarar
-    if self.enmascarar and len(all_tags) > 0:
-      masked_texto  = final_texto[:all_tags['start'][0]] + all_tags['tag'][0]  
+    if len(all_tags) > 0:
+      masked_texto  = final_texto[:all_tags['start'][0]] + all_tags['tag'][0]
       for x in range(len(all_tags)-1):
         masked_texto += final_texto[all_tags['end'][x]:all_tags['start'][x+1]] + all_tags['tag'][x+1]
-      final_texto = masked_texto
-      
-    return final_texto, all_tags
+
+      masked_texto += final_texto[all_tags['end'][len(all_tags)-1]:]
+
+    return masked_texto, all_tags
